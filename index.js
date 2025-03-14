64120745071a2d4ea236bac1973203de3f407f6c
unknown <luiz.g.silva4@gmail.com>
2025-03-14 20:34:25 -0300

    Feat - Create the initial estructure from persons and students - Create the Person class

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+app.use(express.json())
 
+class Person {
+    constructor(id, name, age){
+        this.id = id,
+        this.name = name,
+        this.age = age
+    }
+
+    // Getters
+    getId() {
+        return this.id
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getAge() {
+        return this.age
+    }
+
+    // Setters
+    setName(value) {
+        this.name = value
+    }
+
+    setAge(value) {
+        this.age = value
+    }
+}
+
+const person = new Person(1, 'Luiz', 10)
+console.log(person)
