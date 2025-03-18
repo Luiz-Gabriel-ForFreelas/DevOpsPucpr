2a51146b2098673e4757573c10f6f62d96ceec55
unknown <luiz.g.silva4@gmail.com>
2025-03-18 12:18:32 -0300

    Fix server.test loop

--- server.test.js
+++ server.test.js
@@ -2,6 +2,18 @@ const request = require('supertest');
 const app = require('./server'); // Importa o app sem iniciar o servidor
 
 describe('Testando rotas do Express', () => {
+    let server;
+
+    beforeAll(() => {
+        // Inicializa o servidor antes de rodar os testes
+        server = app.listen(8080);
+    });
+
+    afterAll(() => {
+        // Fecha o servidor depois que os testes terminarem
+        server.close();
+    });
+
     test('GET / deve retornar "Hello, World!"', async () => {
         const response = await request(app).get('/');
         expect(response.status).toBe(200);
