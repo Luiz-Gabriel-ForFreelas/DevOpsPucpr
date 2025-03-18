f462c08435e7cea8fa9d39338e55de09f5be53c2
unknown <luiz.g.silva4@gmail.com>
2025-03-18 12:26:14 -0300

    Fix server.jest run

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const express = require('express');
 const PORT = 8080;
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
-})
+    res.send('Hello, World!');
+});
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
 
 module.exports = app; // Exporta o app para ser usado nos testes
-
-app.listen(PORT)
