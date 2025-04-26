9255d0ae9392f6ca3859674391ae34933ec6bd72
unknown <luiz.g.silva4@gmail.com>
2025-04-26 17:49:01 -0300

    Fix POST /api/alunos route and serverjs error

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const PORT = 8080;
 const app = express();
 
-const alunos = Array(
+app.use(express.json());
+
+const alunos = [
     {id: 1, nome: "Luiz", idade: 21, curso: "Análise de Sistemas"},
     {id: 2, nome: "Maria", idade: 20, curso: "Farmácia"}
-)
+]
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -15,18 +17,18 @@ app.get('/api/alunos', (req, res) => {
     res.send(alunos)
 })
 
-app.get('/api/alunos/:id', (rec, res) => {
+app.get('/api/alunos/:id', (req, res) => {
     const aluno = alunos.find(a => a.id === parseInt(rec.params.id))
     if(!aluno) {res.status(404).send('Aluno não encontrado')}
     res.send(aluno)
 })
 
-app.post('/api/alunos', (rec, res) => {
+app.post('/api/alunos', (req, res) => {
     const aluno = {
         id: alunos.length + 1,
-        nome: rec.body.nome,
-        idade: rec.body.idade,
-        curso: rec.body.curso
+        nome: req.body.nome,
+        idade: req.body.idade,
+        curso: req.body.curso
     }
     alunos.push(aluno)
     res.send(alunos)
